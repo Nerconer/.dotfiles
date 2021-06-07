63dcf5d5ea4e86bbf474eddd32b3ace4841e2f93
David Solà <funnynike4@gmail.com>
2021-06-07 10:39:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ function setup_backup() {
 }
 
 function setup_symlinks() {
-
+	echo "Settinh up symbolic links"
 }
 
 function setup_git() {
@@ -30,16 +30,27 @@ function setup_git() {
 }
 
 function setup_homebrew() {
-
+	echo "Setting up Homebrew"
+	
+	if test ! "$(command -v brew)"; then
+		echo "Homebrew not installed. Installing..."
+		# Run as a login shell (non-interactive) so that the script doesn't pause for user input
+		curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash --login
+    	fi
+	
+	# install brew dependencies from Brewfile
+    	brew bundle
 }
 
 function setup_shell() {
-
+	echo "Configurating Shell"
+	
+	
 }
 
 case "$1" in
 	backup)
-		echo "backup"
+		setup_backup
 		;;
 	link)
 		setup_symlinks
